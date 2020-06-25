6fdf81972d6bae05c1a1d4e669d7bacc102d9018
yves-christol <yves.christol@gmail.com>
2020-06-25 18:00:08 +0800

    2 files modified

--- base/initBase.js
+++ base/initBase.js
@@ -1,10 +1,10 @@
-import React from 'react';
 import firebase from 'firebase';
 import firebaseConfig from './firebaseConfig';
 
-
 const initBase = (setGlobalState) => {
+  console.log('Tata');
   if (!firebase.apps.length) {
+    console.log('Titi');
     firebase.initializeApp(firebaseConfig);
     setGlobalState('auth', firebase.auth());
     setGlobalState('base', firebase.firestore());
--- index.js
+++ index.js
@@ -5,10 +5,10 @@ import initBase from './base/initBase';
 import App from './components/App';
  
 const { useGlobalState, getGlobalState, setGlobalState } = createGlobalState({ base: null, auth: null, theme: 'dark' });
+console.log('Toto '+getGlobalState('theme'));
 
 initBase(setGlobalState);
 
-console.log('Toto '+getGlobalState('theme'));
 console.log('Toto '+getGlobalState('base'));
 setTimeout(() => { console.log('Toto '+getGlobalState('base')) }, 5000);
 
